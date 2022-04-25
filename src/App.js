955dcc3255977ef8654e1e0d55df72f862ec6c39
Jetom88 <shinhro3o@gmail.com>
2022-04-25 16:03:21 +0900

    클래스형으로 ref 사용하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ValidationSample />
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>밑으루</button>
       </div>
     );
   }
