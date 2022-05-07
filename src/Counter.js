8e2ce6d0d404f0faf70b24f2ad7ad661554a69f3
Jetom88 <shinhro3o@gmail.com>
2022-05-07 22:02:34 +0900

    useReducer 사용 방법

--- src/Counter.js
+++ src/Counter.js
@@ -1,65 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useReducer } from "react";
+
+//useReducer는 state보다 더 다양한 상황에서 다양한 상태를 다른 값으로 업데이트할 때 사용하는 hook
+//리듀서는 현재 상태, 업데이트를 위해 필요한 정보를 담은 액션 값을 전달받아 새로운 상태를 반환하는 함수
+//새로운 상태를 만들 때는 불변성 지키기
+
+function reducer(state, action) {
+  const { value } = state;
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: value + 1 };
+    case "DECREMENT":
+      return { value: value - 1 };
+    default:
+      //해당 안되면 기존 상태 반환
+      return state;
+  }
+}
 
 const Counter = () => {
-  const [form, setForm] = useState({
-    username: "",
-    nickname: "",
-  });
-
-  //구조분해할당
-  const { username, nickname } = form;
-
-  //마운트될 때만 실행
-  //   useEffect(() => {
-  //     console.log({ username, nickname });
-  //   }, []);
-
-  //특정 값이 업데이트될 때만
-  //   useEffect(() => {
-  //     console.log({ username, nickname });
-  //   }, [username, nickname]);
-
-  //언마운트되기 전이나 업데이트되기 직전에 수행하고싶다면 cleanup 함수 반환
-  //   useEffect(() => {
-  //     console.log("effect");
-  //     console.log(username);
-  //     return () => {
-  //       console.log("cleanup");
-  //       console.log(username);
-  //     };
-  //   }, [username]);
-
-  //언마운트될 때 cleanup을 호출하고 싶을때
-  useEffect(() => {
-    console.log("effect");
-    console.log(username);
-    return () => {
-      console.log("unmount");
-      console.log(username);
-    };
-  }, []);
-
-  const onChange = (e) => {
-    const changeForm = {
-      ...form,
-      [e.target.name]: e.target.value,
-    };
-    setForm(changeForm);
-  };
-
+  //useReducer의 첫 번째는 함수, 두 번째는 리듀서 기본 값
+  //state는 현재 가리키고 있는 상태, dispatch는 액션을 발생시키는 함수
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+  const { value } = state;
   return (
-    <>
-      <div>
-        <input name="username" onChange={onChange} value={username} />
-        <input name="nickname" onChange={onChange} value={nickname} />
-      </div>
-      <div>
-        <b>이름:</b> {username}
-      </div>
-      <div>
-        <b>닉네임:</b> {nickname}
-      </div>
-    </>
+    <div>
+      <p>
+        현재 카운터 값은 <b>{value}</b>입니다.
+      </p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
+    </div>
   );
 };
 
